95278fd8e661fe23224427598ee94246d7f8b470
Naumov <naumov.a.n@mail.ru>
2025-02-07 21:37:21 +0700

    home work1

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+const int led_pin{39};
+bool light_on{false};
+short numbersCliks{0};
+const short max_numbers_cliks{3};
+
+bool lastButtonState = LOW;
+
+void setup() 
+{
+    pinMode(led_pin, OUTPUT);
+}
+
+void loop() 
+{
+    bool currentButtonState = digitalRead(17);
+
+    if(currentButtonState != lastButtonState)
+    {
+        delay(50);
+        if(currentButtonState == HIGH)
+        {
+            numbersCliks++;
+            if(numbersCliks >= max_numbers_cliks)
+            {
+                digitalWrite(led_pin, HIGH);
+                delay(1000);
+                digitalWrite(led_pin, LOW);
+                numbersCliks = 0;
+            }
+        }
+    }
+    lastButtonState = currentButtonState;
+}
