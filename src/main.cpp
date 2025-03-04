8ba2cf1df733a184349217ef92f9edbd8a1216a9
Naumov <naumov.a.n@mail.ru>
2025-03-04 23:18:26 +0700

    les-successful

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,38 @@
 #include <Arduino.h>
+#include <string>
 
 /*инициализация переменных*/
 const int led_pin{39};
-bool light_on{false};
 short numbersCliks{0};
-const short max_numbers_cliks{3};
+const short max_numbers_cliks{1};
+int power{255};
+std::string buffer = "";
 
 bool lastButtonState = LOW;
 
 void setup() 
 {
     pinMode(led_pin, OUTPUT);
+    Serial.begin(115200);
 }
 
 void loop() 
 {
-    
+    if(Serial.available() > 0)
+    {
+        buffer = "";
+        while (Serial.available() > 0)
+        {
+            buffer += std::string(1, char(Serial.read()));
+            delay(500);
+        }
+
+        power = std::stoi(buffer);
+
+        Serial.print("Светодиод: ");
+        Serial.println(power);
+    }
+
     bool currentButtonState = digitalRead(17);
 
     /*проверка нажатия кнопки, без проверки неработает*/
@@ -27,9 +44,9 @@ void loop()
             numbersCliks++;
             if(numbersCliks >= max_numbers_cliks)
             {
-                digitalWrite(led_pin, HIGH);
+                analogWrite(led_pin, power);
                 delay(1000);
-                digitalWrite(led_pin, LOW);
+                analogWrite(led_pin, 0);
                 numbersCliks = 0;
             }
         }
