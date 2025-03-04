3c54fcfdace7e90088593701336bd379bbe5869e
Naumov <naumov.a.n@mail.ru>
2025-03-04 09:22:12 +0700

    les

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,71 @@
 #include <Arduino.h>
-#include <string>
 
-/*инициализация переменных*/
-const int led_pin{39};
-bool light_on{false};
-short numbersCliks{0};
-const short max_numbers_cliks{3};
-
-int power = 255;
-std::string buffer = "";
+/* Инициализация переменных */
+const int led_pin = 39;
+const int button_pin = 17;
+const int pwmChannel = 0;   // PWM-канал
+const int pwmFreq = 5000;   // Частота ШИМ
+const int pwmResolution = 8; // Разрешение 8 бит (0-255)
 
 bool lastButtonState = LOW;
+short numbersClicks = 0;
+const short maxNumbersClicks = 3;
+
+int power = 255;
+String buffer = "";
 
 void setup() 
 {
     pinMode(led_pin, OUTPUT);
+    pinMode(button_pin, INPUT_PULLUP);
     Serial.begin(115200);
+
+    // Настройка PWM
+    ledcSetup(pwmChannel, pwmFreq, pwmResolution);
+    ledcAttachPin(led_pin, pwmChannel);
 }
 
 void loop() 
 {
-    if(Serial.available() > 0)
+    if (Serial.available() > 0)
     {
         buffer = "";
-        while(Serial.available() > 0)
+        while (Serial.available() > 0)
         {
-            buffer += std::string(1, char(Serial.read()));
-            delay(500);
+            char c = Serial.read();
+            if (isDigit(c) || c == '\n') // Фильтрация символов
+                buffer += c;
+            delay(50);
+        }
+
+        if (buffer.length() > 0)
+        {
+            power = buffer.toInt();
+            Serial.print("Received power: ");
+            Serial.println(power);
         }
     }
 
-    power = std::stoi(buffer);
-    Serial.print("Recived power: ");
-    Serial.println(power);
-            
+    bool currentButtonState = digitalRead(button_pin);
 
-    bool currentButtonState = digitalRead(17);
-
-    /*проверка нажатия кнопки, без проверки неработает*/
-    if(currentButtonState != lastButtonState)
+    /* Проверка нажатия кнопки */
+    if (currentButtonState != lastButtonState)
     {
-        delay(50);
-        if(currentButtonState == HIGH)
+        delay(50); // Антидребезг
+        if (currentButtonState == LOW) // Кнопка нажата (если PULLUP)
         {
-            numbersCliks++;
-            if(numbersCliks >= max_numbers_cliks)
+            numbersClicks++;
+            Serial.print("Click count: ");
+            Serial.println(numbersClicks);
+
+            if (numbersClicks >= maxNumbersClicks)
             {
-                analogWrite(led_pin, power);
+                Serial.println("LED ON");
+                ledcWrite(pwmChannel, power);
                 delay(1000);
-                analogWrite(led_pin, 0);
-                numbersCliks = 0;
+                Serial.println("LED OFF");
+                ledcWrite(pwmChannel, 0);
+                numbersClicks = 0;
             }
         }
     }
